c0185fd3d4672de34cd13231233f853b31cbe847
Matt Diebolt <mdiebolt@gmail.com>
2014-01-09 18:48:31 -0600

    cleaning up some file index stuff

--- webstub.go
+++ webstub.go
@@ -8,12 +8,21 @@ import (
   "net/http"
   "os"
   "strings"
+  "time"
 )
+
+type Response map[string]interface{}
+
+var global struct {
+  Gists map[string] *Gist
+}
 
 type Gist struct {
   Id string
   Description string
   User User
+  FileIndex int         // Not derived from JSON, used to loop over gist file content
+  RefreshedAt time.Time // Not derived from JSON, used to expire Gist cache
   Files map[string]File
 }
 
@@ -32,27 +41,27 @@ func parseRequest(r *http.Request) (gistId string) {
   return strings.Join(path, "")
 }
 
-func getGist(id string) (Gist, error) {
+func getGist(id string) (*Gist, error) {
+  fmt.Println("Making API request")
   url := "https://api.github.com/gists/" + id
 
   resp, err := http.Get(url)
   panicError(err)
-
   defer resp.Body.Close()
+
   body, err := ioutil.ReadAll(resp.Body)
   panicError(err)
 
   var data Gist
   err = json.Unmarshal(body, &data)
-  if err != nil {
-    fmt.Printf("%T\n%s\n%#v\n", err, err, err)
-    switch v := err.(type) {
-      case *json.SyntaxError:
-        fmt.Println(string(body[v.Offset-40:v.Offset]))
-    }
-  }
+  panicError(err)
 
-  return data, err
+  // Cache gist so that we don't have to
+  // use tons of GitHub API requests
+  data.RefreshedAt = time.Now()
+  global.Gists[id] = &data
+
+  return &data, err
 }
 
 func panicError(err error) {
@@ -66,40 +75,70 @@ func handlerIcon(w http.ResponseWriter, r *http.Request) {}
 func handlerRoot(w http.ResponseWriter, r *http.Request) {
   if r.Method == "GET" {
     gistId := parseRequest(r)
-    nonJSONFile := false
 
-    if _, okay := global.FileIndex[gistId]; !okay {
-      global.FileIndex[gistId] = 0
+    var hasKey bool
+    var err error
+    output, hasKey := global.Gists[gistId]
+
+    if hasKey {
+      now := time.Now()
+      delta := now.Sub(output.RefreshedAt)
+
+      // blow away the cached Gist
+      // if it's older than 2 hours
+      if (int(delta.Minutes()) > 120) {
+        delete(global.Gists, gistId)
+
+        // refresh the data and cache it
+        output, err = getGist(gistId)
+        panicError(err)
+      }
     }
 
-    output, err := getGist(gistId)
-    panicError(err)
-
-    res := ""
-    index := 0
-    for _, v := range output.Files {
-      if v.Type != "JSON" {
-        nonJSONFile = true
-      }
-
-      if global.FileIndex[gistId] == index {
-        res = v.Content
-      }
-
-      index += 1
+    if !hasKey {
+      output, err = getGist(gistId)
+      panicError(err)
     }
 
     w.Header().Set("Content-Type", "application/json")
 
-    if nonJSONFile {
-      fmt.Fprint(w, Response{"message": "One or more of the files in this Gist aren't JSON"})
-    } else if len(output.Files) == 0 {
+    length := len(output.Files)
+    if length == 0 {
       fmt.Fprint(w, Response{"message": "This Gist doesn't have any files"})
+    } else if hasNonJsonFile(output.Files) {
+      fmt.Fprint(w, Response{"message": "One or more of the files in this Gist aren't JSON"})
     } else {
-      global.FileIndex[gistId] = (global.FileIndex[gistId] + 1) % (len(output.Files))
-      fmt.Fprint(w, res)
+      output.FileIndex = (output.FileIndex + 1) % length
+      fmt.Fprint(w, currentFileContent(output.Files, output.FileIndex))
     }
   }
+}
+
+func currentFileContent(files map[string]File, index int) string {
+  output := ""
+  i := 0
+
+  for _, v := range files {
+    if index == i {
+      output = v.Content
+    }
+
+    i += 1
+  }
+
+  return output
+}
+
+func hasNonJsonFile(files map[string]File) bool {
+  output := false
+
+  for _, v := range files {
+    if v.Type != "application/json" {
+      output = true
+    }
+  }
+
+  return output
 }
 
 func port() string {
@@ -110,26 +149,18 @@ func port() string {
   return os.Getenv("PORT")
 }
 
-var global struct {
-  FileIndex map[string]int
-}
-
-type Response map[string]interface{}
-
-func (r Response) String() (s string) {
+func (r Response) String() string {
   b, err := json.Marshal(r)
   if err != nil {
-    s = ""
-    return
+    return ""
   }
-  s = string(b)
-  return
+  return string(b)
 }
 
 func main() {
   port := port()
 
-  global.FileIndex = make(map[string]int, 100)
+  global.Gists = make(map[string]*Gist, 100)
 
   http.HandleFunc("/favicon.ico", handlerIcon)
   http.HandleFunc("/", handlerRoot)
